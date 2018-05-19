022e873c6345e712d904f20cedf186a69db84122
hp <hp@192.168.1.5>
2018-05-19 23:08:07 +0530

    Map coding commit

---
+++ src/main/java/map/MapPractice.java
@@ -0,0 +1,26 @@
+package map;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class MapPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Map<Character, Integer> mp=new LinkedHashMap<Character,Integer>();
+		String st="I will win the Interview";
+		char[] stChar=st.toCharArray();
+		for (char stc: stChar) {
+			if (mp.containsKey(stc)) {
+				mp.put(stc, mp.get(stc)+1);
+			}
+			else {
+				mp.put(stc, 1);
+			}	
+		}
+		for (Entry<Character, Integer> c : mp.entrySet()) {
+			System.out.println(c);
+		}
+	}
+}
