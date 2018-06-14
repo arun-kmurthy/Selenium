c53075d5d44e700e33e407c651586bedb4b2f4a0
hp <hp@192.168.1.7>
2018-06-14 08:48:38 +0530

    MapSort

---
+++ src/main/java/map/MapSort.java
@@ -0,0 +1,28 @@
+package map;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+public class MapSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Map<Integer,String > unsorted=new LinkedHashMap<Integer,String>();
+		unsorted.put(21, "Twenty One");
+		unsorted.put(41, "Forty One");
+		unsorted.put(31, "Thirty One");
+		for (Entry <Integer,String> mp:unsorted.entrySet()) {
+
+			System.out.println("The Unsorted Map is: "+mp.getKey()+" "+mp.getValue());
+		}
+		Map <Integer,String> sorted=new TreeMap<Integer,String>(unsorted);
+		sorted.putAll(unsorted);
+
+		for (Entry <Integer,String> mp:sorted.entrySet()) {
+
+			System.out.println("The sorted Map is: "+mp.getKey()+" "+mp.getValue());
+		}
+	}
+}
